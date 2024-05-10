ee04ae3002fab2795660982677daab21035244b2
RATHEESH G KUMAR <ratheeshgopinadhkumar@gmail.com>
2024-05-10 18:17:39 +0530

    changes made

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
+import "fmt"
 
-func Palindrome(s string)bool{
-	if s = ""{
-		return -1
+func Palindrome(s string) bool {
+	if len(s) == 0 || len(s)==1 {
+		return true
 	}
-	return Palindrome(s[i] = s[len-i-1])
+	if s[0] == s[len(s)-1]{
+		return Palindrome(s[1:len(s)-1])
+	}
+	return false	
+}
+
+func main(){
+	s := "malayalam"
+	r := "rat"
+	fmt.Println("Number is palindrome",Palindrome(s))
+	fmt.Println("Number is palindrome",Palindrome(r))
 }
